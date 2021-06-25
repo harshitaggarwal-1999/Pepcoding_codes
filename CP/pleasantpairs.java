57f4f8d3e3d65f9493f76dabeab978733d8fe8c4
harshitaggarwal-1999 <harshitagg0310@gmail.com>
2021-06-26 03:11:58 +0530

    26-06-2021

---
+++ CP/pleasantpairs.java
@@ -0,0 +1,263 @@
+/*
+>>>>>>>>>>>>>>>>>>>>>>>>HARSHIT AGGARWAL<<<<<<<<<<<<<<<<<<<<
+>>>>>>>>>>>>MAHARAJA AGRASEN INSTITUE OF TECHNOLOGY<<<<<<<<<
+
+>> YOU CAN DO THIS <<
+*/
+import java.util.*;
+import java.io.*;
+
+public class pleasantpairs {
+    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> FAST I/O <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            try {
+                br = new BufferedReader(new FileReader("input.txt"));
+                PrintStream out = new PrintStream(new FileOutputStream("output.txt"));
+                System.setOut(out);
+            }
+            catch (Exception e) {
+                br = new BufferedReader(
+                new InputStreamReader(System.in));
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        float nextFloat(){
+            return Float.parseFloat(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> MATHS VALUES <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+    // static int INF = 998244353;
+    static int INF = (int) 1e9 + 7;
+    static int MAX = Integer.MAX_VALUE; // static int MAX = 2147483647
+    static int MIN = Integer.MIN_VALUE; // static int MIN = -2147483647
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> PAIR <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    static class Pair implements Comparable<Pair> {
+        int first, second;
+
+        public Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+        public int compareTo(Pair o) {
+            return this.first- o.first;
+        }
+
+    }
+
+    static class LongPair {
+        long first;
+        long second;
+
+        LongPair(long a, long b) {
+            this.first = a;
+            this.second = b;
+        }
+    }
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> GCD <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    public static long gcd(long a, long b) {
+        if (a == 0)
+            return b;
+        return gcd(b % a, a);
+    }
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> LOWER BOUND <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+    public static int LowerBound(long a[], long x) { // x is the target value or key
+
+        int l = -1, r = a.length;
+        while (l + 1 < r) {
+            int m = (l + r) >>> 1;
+            if (a[m] >= x)
+                r = m;
+            else
+                l = m;
+        }
+        return r;
+    }
+
+    public static int LowerBound(int a[], int x) { // x is the target value or key
+
+        int l = -1, r = a.length;
+        while (l + 1 < r) {
+            int m = (l + r) >>> 1;
+            if (a[m] >= x)
+                r = m;
+            else
+                l = m;
+        }
+        return r;
+    }
+
+    static int LowerBoundList(ArrayList<Integer> a, int x) { // x is the target value or key
+        int l = -1, r = a.size();
+        while (l + 1 < r) {
+            int m = (l + r) >>> 1;
+            if (a.get(m) >= x)
+                r = m;
+            else
+                l = m;
+        }
+        return r;
+    }
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> SIEVE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    static boolean[] prime;
+
+    public static void sieveOfEratosthenes(int n) {
+        prime = new boolean[n + 1];
+        for (int i = 0; i < n; i++)
+            prime[i] = true;
+
+        for (int p = 2; p * p <= n; p++) {
+
+            if (prime[p] == true) {
+
+                for (int i = p * p; i <= n; i += p)
+                    prime[i] = false;
+            }
+        }
+    }
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> UPPERBOUND <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    public static int UpperBound(long a[], long x) {// x is the key or target value
+        int l = -1, r = a.length;
+        while (l + 1 < r) {
+            int m = (l + r) >>> 1;
+            if (a[m] <= x)
+                l = m;
+            else
+                r = m;
+        }
+        return l + 1;
+    }
+
+    public static int UpperBound(int a[], int x) {// x is the key or target value
+        int l = -1, r = a.length;
+        while (l + 1 < r) {
+            int m = (l + r) >>> 1;
+            if (a[m] <= x)
+                l = m;
+            else
+                r = m;
+        }
+        return l + 1;
+    }
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> LCM <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    public static long lcm(long a, long b) {
+        return (a * b) / gcd(a, b);
+    }
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> SWAP <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    public static void swap(int[] arr, int i, int j) {
+        if (i != j) {
+            arr[i] ^= arr[j];
+            arr[j] ^= arr[i];
+            arr[i] ^= arr[j];
+        }
+    }
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ARRAYINPUT <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+    public static long[] nextLongArray(int n) {
+        long[] a = new long[n];
+        for (int i = 0; i < n; i++)
+            a[i] = scn.nextLong();
+        return a;
+    }
+
+    public static int[] nextIntArray(int n) {
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scn.nextInt();
+        }
+        return a;
+    }
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> SMALLEST DIVISOR <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    public static int smallest_divisor(int n) {
+
+        int i;
+        for (i = 2; i <= Math.sqrt(n); ++i) {
+            if (n % i == 0) {
+                return i;
+            }
+        }
+        return n;
+    }
+
+    public static FastReader scn = new FastReader();
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>> CODE STARTS HERE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+    public static void main(String[] args) throws Exception {
+        int t = scn.nextInt();
+        while (t-- > 0) {
+            solver();
+        }
+    }
+
+    public static void solver() {
+        int n = scn.nextInt();
+        Pair[] arr = new Pair[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = new Pair(scn.nextInt(), i+1);
+        }
+
+        Arrays.sort(arr, new Comparator<Pair>() {
+            @Override public int compare(Pair p1, Pair p2)
+            {
+                return p1.second - p2.second;
+            }
+        });
+        int count = 0;
+        for(int i = 0 ; i < n-1; i++){
+            if(arr[i].second + arr[i+1].second == arr[i].first*arr[i+1].first){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+//-------------------------------- HO JA BHAI ----------------------------------------------------
+}
