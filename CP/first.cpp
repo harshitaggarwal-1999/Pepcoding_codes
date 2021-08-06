78f23a8cf9b23a2895771cb8bb9a5bfc73af3504
Harshit Aggarwal <harshitagg0310@gmail.com>
2021-08-06 16:40:48 +0530

    6-08-2021

--- CP/first.cpp
+++ CP/first.cpp
@@ -1,47 +1,82 @@
-// you can do it.:)
-#include<iostream>
-// #include<algorithm>
-// #include<string>
-// #include<unordered_maps>
-// #include<climit>
-// #include<bits/stdc++.h>
+//you never give up.
+#include <bits/stdc++.h>
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#define ll long long
+#define pb push_back
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define set_bits __builtin_popcountll
+#define v32 vector<int>
+#define v64 vector<ll>
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
 using namespace std;
+/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+// void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+// void _print(ull t) {cerr << t;}
 
-typedef long long ll;
-void ans(){
-    // code here
-    int n,k;
-    if(k > (n-1)/2)cout << -1 << endl;
-    else{
-        int[] arr = new int[n];
-        for(int i= 0; i < n; i++){
-            arr[i] = i+1;
-        }
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+// void _print(pbds v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
-        for(int i = n-1; i >= 2; i=-2){
-            int temp = arr[i];
-            arr[i] = arr[i-1];
-            arr[i-1] = temp;
-        }
 
-        for(int i = 0 ; i < n; i++){
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-    }
 
-}
+void solve()
+{
+    //write code here
+    
+}  
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
 
-    int t;
+
+int main()
+{  
+    /*------------------------------------*/ 
+    #ifndef ONLINE_JUDGE 
+    freopen("input.txt", "r", stdin); 
+    freopen("output.txt", "w", stdout);
+freopen("Error.txt", "w", stderr);
+   #endif // ONLINE_JUDGE
+    /*----------------------------------*/
+    fastio();
+    int t=1;
     cin >> t;
-    while(t--){
-        ans();
-
-    }
-    return 0;
+    while (t--)solve();   
 }
--- CP/harshittt.java
+++ CP/harshittt.java
@@ -7,7 +7,7 @@ FROM-> MAHARAJA AGRASEN INSTITUE OF TECHNOLOGY
 import java.util.*;
 import java.io.*;
 
-public class new1 {
+public class harshittt {
     public static void main(String[] args) throws Exception {
         int t = scn.nextInt();
         while (t-- > 0) {
@@ -18,7 +18,7 @@ public class new1 {
     public static void solver() {
         int n = scn.nextInt();
         System.out.println(n);
-        
+        // Debug.dbg(n);
 
     }
     //-------------------------------- HO JA BHAI ----------------------------------------------------
